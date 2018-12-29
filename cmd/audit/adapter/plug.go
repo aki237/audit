660591a3759ca20e76b2bd481656283ca4afd844
Akilan Elango <akilan1997@gmail.com>
2018-12-29 23:36:26 +0530

    Added some documentation to the types and the constant

--- cmd/audit/adapter/plug.go
+++ cmd/audit/adapter/plug.go
@@ -1,9 +1,16 @@
 package adapter
 
+// Plug is used to denote a set of adapters that do a particular type of collection
 type Plug uint
 
 const (
+	// OS plug types only fetch OS based resources like RAM, CPU Usage,
+	// process usage and et.,
 	OS Plug = iota + 1
+
+	// NETWORK plug types only fetch network based resources
 	NETWORK
+
+	// LOG is used to collect all the logs of the current process
 	LOG
 )
